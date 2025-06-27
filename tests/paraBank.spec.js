795f3b15fa7dfca34b28acdb5eacc77771b0d9a8
MantvydasIvaskevicius <mmivaskevicius@gmail.com>
2025-06-28 00:36:57 +0300

    w

--- tests/paraBank.spec.js
+++ tests/paraBank.spec.js
@@ -253,6 +253,9 @@ test.describe('Account services functions', () => {
 
         
         
+
+
+        
         await helper.updateProfile(page);
 
         await expect(page.locator(selectors.successUpdt)).toBeVisible();
