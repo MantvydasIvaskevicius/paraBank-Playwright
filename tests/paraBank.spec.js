14733f1c7a10e6703e0fceee82c91bbfe52fef09
MantvydasIvaskevicius <mmivaskevicius@gmail.com>
2025-07-15 21:59:13 +0300

    w

--- tests/paraBank.spec.js
+++ tests/paraBank.spec.js
@@ -253,8 +253,12 @@ test.describe('Account services functions', () => {
 
         await expect(page.locator(selectors.successUpdt)).toBeVisible();
         await expect(page.locator(selectors.successUpdt)).toHaveText('Your updated address and phone number have been added to the system.');
-    
+    });
 
+
+
+
+    
     test('Request credit successfully', async ({ page }) => {
         await helper.logIn(page);
 
